13bd82df574a5e055872a68d02c5db0b7464be04
Mario <mario.quitt@gmail.com>
2025-05-10 16:04:21 +0200

    fix

--- dimm.py
+++ dimm.py
@@ -9,20 +9,24 @@ try:
     while True:
         # Von langsam (1s) zu schnell (0.01s) -> Beschleunigung
         delay = 1.0
-        while delay > 0.01:
+        while delay >= 0.01:
             GPIO.output(LED_PIN, GPIO.HIGH)
             time.sleep(delay)
             GPIO.output(LED_PIN, GPIO.LOW)
             time.sleep(delay)
-            delay -= 0.05  # Sanft schneller werden
+            delay -= 0.05
+            if delay < 0.01:
+                delay = 0.01  # Untere Grenze setzen
 
         # Von schnell (0.01s) wieder zu langsam (1s) -> Verlangsamung
-        while delay < 1.0:
+        while delay <= 1.0:
             GPIO.output(LED_PIN, GPIO.HIGH)
             time.sleep(delay)
             GPIO.output(LED_PIN, GPIO.LOW)
             time.sleep(delay)
-            delay += 0.05  # Sanft langsamer werden
+            delay += 0.05
+            if delay > 1.0:
+                delay = 1.0  # Obere Grenze setzen
 
 except KeyboardInterrupt:
     GPIO.cleanup()
